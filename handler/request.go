91bbfb2394a6176a074235f88a79b68b66dc8b73
Pedro Franca <nfranca.tec1@gmail.com>
2025-08-27 20:06:47 +0100

    feat: add request and response handlers

---
+++ handler/request.go
@@ -0,0 +1,19 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param %s (type%s) is required", name, typ)
+}
+
+// CreateTodo
+type CreateTodoRequest struct {
+	Text string `json:"text"`
+}
+
+func (r *CreateTodoRequest) Validate() error {
+	if r.Text == "" {
+		return errParamIsRequired("text", "string")
+	}
+	return nil
+}
